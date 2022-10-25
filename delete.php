c88425b0493937406d0eb85f168d388973907b5b
Wemerson <73254098+wemersonm@users.noreply.github.com>
2022-10-24 21:57:56 -0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,8 +4,6 @@ require_once 'config/connection.php';
 
 $id = intval($_GET['id']);
 
-
-
 $stmt = $conn->prepare("DELETE FROM `cars` WHERE id = ?");
 if($stmt->execute((array($id)))){
     header("location: index.php");
